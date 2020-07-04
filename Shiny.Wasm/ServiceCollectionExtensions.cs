15824e4acb02ce8f37af3a844ce823ceb19a90c2
Allan Ritchie <allan.ritchie@gmail.com>
2020-07-04 16:44:28 -0400

    Minor

--- Shiny.Wasm/ServiceCollectionExtensions.cs
+++ Shiny.Wasm/ServiceCollectionExtensions.cs
@@ -1,16 +1,34 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.JSInterop;
+using Shiny.Infrastructure;
+using Shiny.Jobs;
 using Shiny.Locations;
 using Shiny.Notifications;
 using Shiny.Sensors;
+using Shiny.Settings;
+using Shiny.Wasm.Core;
 using Shiny.Wasm.Locations;
 using Shiny.Wasm.Notifications;
 using Shiny.Wasm.Sensors;
+
 
 namespace Shiny.Wasm
 {
     public static class ServiceCollectionExtensions
     {
+        public static void UseShiny(this IServiceCollection services)
+        {
+            // TODO: figure out startup and bindable services
+            services.AddSingleton(x => (IJSInProcessRuntime)x.Resolve<IJSRuntime>());
+            services.AddSingleton<ISerializer, BlazorSerializer>();
+            services.AddSingleton<IEnvironment, EnvironmentImpl>();
+            services.AddSingleton<ISettings, SettingsImpl>();
+            services.AddSingleton<AppStateManagerImpl>();
+            services.AddSingleton<IJobManager, JobManager>();
+        }
+
+
         public static void UseGps(this IServiceCollection services)
         {
             services.TryAddSingleton<IGpsManager, GpsManagerImpl>();
@@ -26,7 +44,7 @@ namespace Shiny.Wasm
         public static void UseGeofencing<TDelegate>(this IServiceCollection services)
         {
             services.UseGps();
-            services.AddSingleton<IGeofenceManager, GpsGeofenceManagerImpl>();
+            //services.AddSingleton<IGeofenceManager, GpsGeofenceManagerImpl>();
         }
 
 
@@ -36,7 +54,16 @@ namespace Shiny.Wasm
             services.AddSingleton<IGyroscope, GyroscopeImpl>();
             services.AddSingleton<IMagnetometer, MagnetometerImpl>();
             services.AddSingleton<IAmbientLight, AmbientLightImpl>();
-            services.AddSingleton<ICompass, SharedCompassImpl>();
+            //services.AddSingleton<ICompass, SharedCompassImpl>();
         }
+
+
+        // TODO: figure out startup and bindable services
+        //services.AddSingleton(x => (IJSInProcessRuntime) x.Resolve<IJSRuntime>());
+        //    services.AddSingleton<ISerializer, BlazorSerializer>();
+        //    services.AddSingleton<IEnvironment, EnvironmentImpl>();
+        //    services.AddSingleton<ISettings, SettingsImpl>();
+        //    services.AddSingleton<AppStateManagerImpl>();
+        //    services.AddSingleton<IJobManager, JobManager>();
     }
 }
